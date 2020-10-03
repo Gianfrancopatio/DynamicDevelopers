175b23fb7196845d43f3b55209f57666171ccb77
Gianfrancopatio <63272514+Gianfrancopatio@users.noreply.github.com>
2020-10-02 22:25:28 -0400

    first commit

---
+++ src/test/java/UI_Selenium/step_defs/ScenarioContext.java
@@ -0,0 +1,71 @@
+package UI_Selenium.step_defs;
+
+import UI_Selenium.pages.LogInPage;
+import UI_Selenium.ui_utils.Selenium_utils;
+import common_utils.ConfigReader;
+import io.cucumber.java.Scenario;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONObject;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ScenarioContext {
+    public Scenario scenario;
+    public WebDriver driver;
+
+    //Classes
+    public LogInPage logInPage;
+    public Selenium_utils selenium_utils;
+
+    //Selenium methods
+    public void initializeClasses(ScenarioContext scenarioContext) {
+        logInPage = new LogInPage(driver);
+        selenium_utils = new Selenium_utils(scenarioContext);
+    }
+
+//    //API
+//    public RequestSpecification request;
+//    public Response response;
+//    public Headers headers;
+//    public JSONObject jsonObject;
+
+    public void initializeDriver() {
+        if (driver == null) {
+            switch (ConfigReader.readProperty("browser", "src/test/resources/properties/configuration.properties")) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    System.out.println("Invalid browser type");
+            }
+        }
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+//    //API Methods
+//    public String getResponseBody(){
+//        return response.getBody().asString();
+//    }
+//
+//    public int getStatusCode(){
+//        return response
+//                .getStatusCode();
+//    }
+//
+//    public String getResponseID(){
+//        return response.body().jsonPath().getString("_id");
+//    }
+
+}
---
+++ src/test/java/runner/ProjectRunner.java
@@ -0,0 +1,24 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"UI_Selenium/step_defs"},
+        plugin = {
+                "json:target/cucumber.json",
+                "rerun:target/re-run.txt"
+        },
+        tags = ""
+        ,dryRun = false
+)
+
+public class ProjectRunner extends AbstractTestNGCucumberTests {
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}

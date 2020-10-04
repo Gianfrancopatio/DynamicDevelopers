954022caacb88f7faf64712dda24783545f2fc74
mariaveret <marina.veretennikova@gmail.com>
2020-10-04 19:13:43 -0400

    adding features

---
+++ src/test/java/UI_Selenium/step_defs/TLA11_LikeButtonTest.java
@@ -0,0 +1,31 @@
+package UI_Selenium.step_defs;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TLA11_LikeButtonTest {
+    ScenarioContext context;
+    public TLA11_LikeButtonTest(ScenarioContext scenarioContext) {
+        context = scenarioContext;
+    }
+    @Then("I verify Like Button is displayed")
+    public void i_verify_like_button_is_displayed() {
+    }
+    @When("I click the Like Button")
+    public void i_click_the_like_button() {
+
+    }
+    @Then("I verify Lie Button is clickable")
+    public void i_verify_lie_button_is_clickable() {
+
+    }
+    @Then("I verify that numbers are increasing")
+    public void i_verify_that_numbers_are_increasing() {
+
+    }
+    @Then("I verify that numbers are decreasing")
+    public void i_verify_that_numbers_are_decreasing() {
+
+    }
+}
+

a7906a9fb4198d8c3c40d91231aa7ae5bb7d70f7
Juan Quiroga Tababary <juanquirogatababary@Juans-MBP.fios-router.home>
2020-10-11 23:49:31 -0400

    commit screenshots

--- src/test/java/UI_Selenium/step_defs/CreateQuestionTest.java
+++ src/test/java/UI_Selenium/step_defs/CreateQuestionTest.java
@@ -33,12 +33,14 @@ public class CreateQuestionTest {
         context.selenium_utils.sendKeys(context.logInPage.email, email);
         context.selenium_utils.sendKeys(context.logInPage.passwordbfSignup, password);
         context.selenium_utils.click(context.logInPage.signInbtn);
+        context.selenium_utils.logInfo("login credentials were accepted", true);
 
     }
 
     @And("User can navigate to Coding Dashboard")
     public void userCanNavigateToCodingDashboard() {
         context.selenium_utils.click(context.createQuestionPage.SelectCoding);
+        context.selenium_utils.logInfo("Coding dashboard window open", true);
 
 
     }
@@ -46,11 +48,13 @@ public class CreateQuestionTest {
     @Then("User clicks on yellow icon")
     public void userClicksOnYellowIcon() {
         context.selenium_utils.click(context.createQuestionPage.clickButton);
+        context.selenium_utils.logInfo("Icon was clickable successfully", true);
     }
 
     @Then("User can input NEW questions under yellow icon")
     public void userCanInputNEWQuestionsUnderYellowIcon() throws InterruptedException {
         context.selenium_utils.click(context.createQuestionPage.EnterQA);
+        context.selenium_utils.logInfo("Was able to input question in bar", true);
 
 
         Thread.sleep(2000);
@@ -63,17 +67,20 @@ public class CreateQuestionTest {
         context.selenium_utils.waitForVisibility(context.createQuestionPage.EnterQA);
         context.selenium_utils.sendKeys(context.createQuestionPage.EnterQA, arg0);
         context.selenium_utils.click(context.createQuestionPage.EnterQA);
+        context.selenium_utils.logInfo("Question was accepted ", true);
     }
 
     @When("User click enter for question")
     public void userClickEnterForQuestion() {
         context.selenium_utils.click(context.createQuestionPage.click_enter);
+        context.selenium_utils.logInfo("Was able to enter question", true);
     }
 
 
     @And("Time stamp is verified")
     public void timeStampIsVerified() {
         Assert.assertTrue(context.createQuestionPage.DisplayStamp.isDisplayed());
+        context.selenium_utils.logInfo("Question displayed with stamp detail ", true);
     }
 
 
